1cc102786ff70c5a971088b5fdd9442dcc54d007
nickbuker <nickbuker@gmail.com>
2017-07-27 09:32:45 -0700

    add vector access

--- tryr2.R
+++ tryr2.R
@@ -1,4 +1,4 @@
-# 2.1
+# 2.1 vectors
 # create a vector with numbers
 c(4, 7, 9)
 
@@ -8,7 +8,7 @@ c('a', 'b', 'c')
 # create a vector with multiple data types
 c(1, T, 'three') # note that all three were converted to strings
 
-# 2.2
+# 2.2 sequence vectors
 # create a sequence vector with start:end notation (inclusive)
 5:9
 
@@ -23,4 +23,32 @@ seq(5, 9, 3)
 9:5
 seq(9,5)
 
-# 2.3
+# 2.3 vector access
+# create a sentence vector
+sentence = c('walk', 'the', 'plank')
+
+# retrieve the 3rd element
+sentence[3]
+
+# retrieve the 1st element
+sentence[1]
+
+# reassign the 3rd element
+sentence[3] = 'dog'
+
+# add a new element
+sentence[4] = 'to'
+
+# access multiple elements using list inside of brackets
+sentence[c(1, 3)]
+
+# retrieve a range of elements
+sentence[2:4]
+
+# add a range of values
+sentence[5:7] = c('the', 'poop', 'deck')
+
+# access the 6th word in the sentence
+sentence[6]
+
+# 2.4 vector names
