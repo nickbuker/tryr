b85934bb59e37d8ed92bd676ba62ffd9a3527f72
nickbuker <nickbuker@gmail.com>
2017-07-27 09:19:07 -0700

    add tryr chapter 2

---
+++ tryr2.R
@@ -0,0 +1,11 @@
+# create a vector with numbers
+c(4, 7, 9)
+
+# create a vector with strings
+c('a', 'b', 'c')
+
+# create a vector with multiple data types
+c(1, T, 'three') # note that all three were converted to strings
+
+# create a sequence vector with start:end notation
+5:9
