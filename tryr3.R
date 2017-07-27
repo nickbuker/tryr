4f2c482f0ae85c1c8cd3207c7f470833d1bb47ea
nickbuker <nickbuker@gmail.com>
2017-07-27 10:17:47 -0700

    add tryr chapter 3

---
+++ tryr3.R
@@ -0,0 +1,12 @@
+# 3.1 matrices
+# create a 3x4 matrix containing all zeros
+matrix(0, 3, 4)
+
+# create a vector to initialize a matrix
+a = 1:12
+
+# print the values of a
+print(a)
+
+# make a matrix containing the values of a
+matrix(a, 3, 4)
