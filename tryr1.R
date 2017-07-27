4e84d534bad0b7657cb12b9ffe2f441e7cba6b72
nickbuker <nickbuker@gmail.com>
2017-07-27 09:15:19 -0700

    update tryr chapter 1

--- tryr1.R
+++ tryr1.R
@@ -1,3 +1,22 @@
+# sum
 sum(1, 3, 5)
 
+# repeat
 rep("Yo ho!", times = 3)
+
+# square root
+sqrt(16)
+
+# get docs for function
+help(sum)
+help(rep)
+
+# get example of a function
+example(min)
+
+# find R scripts
+list.files()
+
+# execute scripts
+source("bottle1.R")
+source("bottle2.R")
