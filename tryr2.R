6465a51323f0279d8133ad5f02bdb619f7bec7b6
nickbuker <nickbuker@gmail.com>
2017-07-27 09:23:50 -0700

    update tryr chapter 2

--- tryr2.R
+++ tryr2.R
@@ -1,3 +1,4 @@
+# 2.1
 # create a vector with numbers
 c(4, 7, 9)
 
@@ -7,5 +8,19 @@ c('a', 'b', 'c')
 # create a vector with multiple data types
 c(1, T, 'three') # note that all three were converted to strings
 
-# create a sequence vector with start:end notation
+# 2.2
+# create a sequence vector with start:end notation (inclusive)
 5:9
+
+# create a sequence with the seq function (inclusive)
+seq(5, 9)
+
+# adjust the step size for seq
+seq(5, 9, 0.5)
+seq(5, 9, 3)
+
+# create a descending vector
+9:5
+seq(9,5)
+
+# 2.3
